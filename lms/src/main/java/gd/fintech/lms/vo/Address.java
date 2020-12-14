93fbbdf906cf81b79280af0dcf1ad44a6c221fff
rigizer <rigizer@gmail.com>
2020-12-14 15:25:30 +0900

    [한재용] address.java Vo 수정
    - id, zipCode, privince, city, town, street, building1, building2 컬럼에 대해
    추가 (ePost 데이터)

--- lms/src/main/java/gd/fintech/lms/vo/Address.java
+++ lms/src/main/java/gd/fintech/lms/vo/Address.java
@@ -6,5 +6,12 @@ import lombok.Data;
 
 @Data
 public class Address {
-	private int		id;		// 주소 고유번호
+	private int		id;			// 주소 고유번호
+	private String	zipCode;	// 우편번호
+	private	String	province;	// 시도
+	private String	city;		// 시군구
+	private String	town;		// 읍면
+	private String	street;		// 도로명
+	private String	building1;	// 건물번호본번
+	private String	building2;	// 건물번호부번
 }
