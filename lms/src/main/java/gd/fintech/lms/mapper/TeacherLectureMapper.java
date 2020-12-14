41dff1fd596320a29476ccf7a398462f7345bbc9
rigizer <rigizer@gmail.com>
2020-12-14 18:42:47 +0900

    [한재용] 강사가 수업중인 강좌 목록을 출력하는 페이지(/auth/teacher/index.jsp)에 대한 백엔드 작업
    - TeacherIndexController.java : 강사 Index 페이지에 대한 Controller 메소드 추가
    (index) 및 페이징
    - TeacherLectureService.java : 강사가 수업중인 강좌 목록을 출력하는 것에 대한 Service
    (getTeacherLectureListByPage, getCountTeacherLecture)
    - TeacherLectureMapper.java : 강사가 수업중인 강좌 목록을 출력하는 것에 대한 Mapper
    (selectTeacherLectureListByPage, countTeacherLecture)
    - TeacherLectureMapper.xml : selectTeacherLectureListByPage,
    countTeacherLecture에 대한 쿼리 작성

---
+++ lms/src/main/java/gd/fintech/lms/mapper/TeacherLectureMapper.java
@@ -0,0 +1,17 @@
+package gd.fintech.lms.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import gd.fintech.lms.vo.Lecture;
+
+@Mapper
+public interface TeacherLectureMapper {
+	// 강사 강좌 목록 페이지에서 목록 페이징
+	List<Lecture> selectTeacherLectureListByPage(Map<String, Object> map);
+	
+	// 강사 강좌 목록 페이지 카운트
+	int countTeacherLecture(String accountId);	// 강사 아이디를 받아옴
+}
