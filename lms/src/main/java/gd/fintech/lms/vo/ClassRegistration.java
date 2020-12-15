b0c3f7c7919984a9e4d7d9678dbc3e018c0404f5
ccb8306 <ccb8306@gmail.com>
2020-12-15 11:32:12 +0900

    [조성현] ClassRegistration VO 오타 수정

--- lms/src/main/java/gd/fintech/lms/vo/ClassRegistration.java
+++ lms/src/main/java/gd/fintech/lms/vo/ClassRegistration.java
@@ -11,7 +11,7 @@ public class ClassRegistration {
 	private int		lectureNo;						// 강좌 고유번호
 	private String	accountId;						// 계정 아이디 (학생)
 	private String	classRegistrationState;			// 강좌 수강 상태 (대기, 수강중, 수료, 과락, 취소)
-	private String	classregistrationPoint;			// 강좌 평점 (0, 1, 2, 3, 4, 5)
+	private String	classRegistrationPoint;			// 강좌 평점 (0, 1, 2, 3, 4, 5)
 	private String	classRegistrationReview;		// 강좌 후기
 	private String	classRegistrationCreatedate;	// 강좌 수강 정보 생성 일시
 }
