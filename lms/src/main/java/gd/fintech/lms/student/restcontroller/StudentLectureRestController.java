8f90706f1bdc6447c93446ce65688e70029b3c34
ccb8306 <ccb8306@gmail.com>
2020-12-16 15:57:04 +0900

    [조성현] 수강신청 기능 추가
    수강 신청시 이미 신청된 강좌인지 비동기로 확인

---
+++ lms/src/main/java/gd/fintech/lms/student/restcontroller/StudentLectureRestController.java
@@ -0,0 +1,31 @@
+package gd.fintech.lms.student.restcontroller;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import gd.fintech.lms.student.restservice.StudentLectureRestService;
+import gd.fintech.lms.vo.ClassRegistration;
+
+@RestController
+public class StudentLectureRestController {
+	@Autowired StudentLectureRestService studentLectureRestService;
+	
+	// 해당 강좌가 이미 수강신청이 되어있는지 체크
+	@PostMapping("auth/student/lecture/checkClassRegistration")
+	public boolean checkClassRegistration(ClassRegistration classRegistration, ServletRequest request) {
+		// 세션에서 로그인 아이디 가져옴
+		HttpSession session = ((HttpServletRequest)request).getSession();
+		String studentId = (String)session.getAttribute("loginId");
+		
+		// classRegistration에 데이터 추가
+		classRegistration.setAccountId(studentId);
+
+		return studentLectureRestService.selectCheckClassRegistration(classRegistration);
+	}
+}
---
+++ lms/src/main/java/gd/fintech/lms/student/restservice/StudentLectureRestService.java
@@ -0,0 +1,21 @@
+package gd.fintech.lms.student.restservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import gd.fintech.lms.student.restmapper.StudentLectureRestMapper;
+import gd.fintech.lms.vo.ClassRegistration;
+
+@Service
+public class StudentLectureRestService {
+	@Autowired StudentLectureRestMapper studentLectureRestMapper;
+	
+	public boolean selectCheckClassRegistration(ClassRegistration classRegistration) {
+		int cnt = studentLectureRestMapper.selectCheckClassRegistration(classRegistration);
+		
+		if (cnt == 0) {
+			return true;
+		}
+		return false;
+	}
+}
