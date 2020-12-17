45c53050c73762bc52e60807cc11d859b99268e3
jooseongkim <wntjd30711@gmail.com>
2020-12-17 16:59:26 +0900

    [김주성] vo.Question에 questionFile 이름을 questionFileList로 수정

--- lms/src/main/java/gd/fintech/lms/vo/Question.java
+++ lms/src/main/java/gd/fintech/lms/vo/Question.java
@@ -9,15 +9,15 @@ import lombok.Data;
 
 @Data
 public class Question {
-	private int		questionNo;				// 질문 고유번호
-	private int		lectureNo;				// 강좌 고유번호
-	private String	accountId;				// 계정 아이디 (학생)
-	private String	questionWriter;			// 작성자 이름 (학생)
-	private String	questionTitle;			// 질문 제목
-	private String	questionContent;		// 질문 내용
-	private String	questionCreatedate;		// 질문 생성 일시
-	private String	questionUpdatedate;		// 질문 업데이트 일시
-	private int		questionCount;			// 질문 조회수
-	private String	questionPassword;		// 질문 비밀번호 (비밀글 설정시 필요)
-	private List<QuestionFile> questionFile;// 질문게시판 파일 리스트
+	private int		questionNo;						// 질문 고유번호
+	private int		lectureNo;						// 강좌 고유번호
+	private String	accountId;						// 계정 아이디 (학생)
+	private String	questionWriter;					// 작성자 이름 (학생)
+	private String	questionTitle;					// 질문 제목
+	private String	questionContent;				// 질문 내용
+	private String	questionCreatedate;				// 질문 생성 일시
+	private String	questionUpdatedate;				// 질문 업데이트 일시
+	private int		questionCount;					// 질문 조회수
+	private String	questionPassword;				// 질문 비밀번호 (비밀글 설정시 필요)
+	private List<QuestionFile> questionFileList;	// 질문게시판 파일 리스트
 }
