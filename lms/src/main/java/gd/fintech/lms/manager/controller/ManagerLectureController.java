50e46fa15e0502a4df8a93ceba4c33b28e1712f8
pad0108 <chkc78@naver.com>
2020-12-17 12:04:13 +0900

    [최형철] 페이징 통일화하기 위해 코드 수정 중

--- lms/src/main/java/gd/fintech/lms/manager/controller/ManagerLectureController.java
+++ lms/src/main/java/gd/fintech/lms/manager/controller/ManagerLectureController.java
@@ -39,7 +39,9 @@ public class ManagerLectureController {
 	public String lectureList(Model model,
 			@PathVariable(name = "currentPage") int currentPage) {
 		int rowPerPage = 10; //한 페이지에 출력할 개수
+		
 		int endPage = managerLectureService.getCountLecture(rowPerPage);//마지막 페이지
+		
 		int beginRow = (currentPage - 1) * rowPerPage; 					// 시작 페이지
 		// 강의 목록 출력
 		Map<String, Object> map = new HashMap<>();
@@ -49,14 +51,17 @@ public class ManagerLectureController {
 		List<Lecture> lectureList = managerLectureService.getLectureListByPage(map);
 		
 		int navPerPage = 10; 											// 네비에 출력할 페이지 개수
+		
 		int navFirstPage = currentPage - (currentPage % navPerPage) + 1;// 네비의 첫번째 페이지
+		
 		int navLastPage = navFirstPage + navPerPage - 1; 				// 네비의 마지막 페이지
+		
 		//현재 페이지가 10으로 나누어 떨어질 때
 		if (currentPage % navPerPage == 0 && currentPage != 0) {
 			navFirstPage = navFirstPage - navPerPage;
 			navLastPage = navLastPage - navPerPage;
 		}
-		
+		// model을 통해 View에 다음과 같은 정보를 보내준다.
 		model.addAttribute("currentPage", currentPage);
 		model.addAttribute("endPage", endPage);
 		
