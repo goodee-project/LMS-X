ac8ea1c2387cfa53c9629707265fcca89676d029
jooseongkim <wntjd30711@gmail.com>
2020-12-22 14:10:26 +0900

    [김주성] QuestionComment 컬럼 이름 수정

--- lms/src/main/java/gd/fintech/lms/vo/QuestionComment.java
+++ lms/src/main/java/gd/fintech/lms/vo/QuestionComment.java
@@ -11,7 +11,7 @@ public class QuestionComment {
 	private int		questionNo;						// 질문 고유번호
 	private String	accountId;						// 계정 아이디 (강사)
 	private String	questionCommentWriter;			// 작성자 이름 (강사)
-	private String	questionCommentContext;			// 질문 댓글 내용
+	private String	questionCommentContent;			// 질문 댓글 내용
 	private String	questionCommentCreatedate;		// 질문 댓글 생성 일시
 	private String	questionCommentUpdatedate;		// 질문 댓글 업데이트 일시
 }
