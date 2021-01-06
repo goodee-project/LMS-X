1343ba2f9d3bdefbebd56c83b7efd39892e2a77f
pad0108 <chkc78@naver.com>
2021-01-06 13:46:27 +0900

    [최형철] 운영자 통계 컨트롤러 주소 수정

--- lms/src/main/java/gd/fintech/lms/manager/restcontroller/ManagerChartRestController.java
+++ lms/src/main/java/gd/fintech/lms/manager/restcontroller/ManagerChartRestController.java
@@ -15,66 +15,66 @@ public class ManagerChartRestController {
 	@Autowired ManagerChartRestService managerChartRestService;
 	
 	// 강의별 성적 평균
-	@GetMapping("auth/manger/chart/AvgGradeByLecture")
+	@GetMapping("auth/manger/chart/avgGradeByLecture")
 	public List<Map<String, Object>> AvgGradeByLecture(){
 		return managerChartRestService.selectAvgGradeByLecture();
 	}
 	// 강의별 최대/최소 성적
-	@GetMapping("auth/manger/chart/MinMaxGradeByLecture")
+	@GetMapping("auth/manger/chart/minMaxGradeByLecture")
 	public List<Map<String, Object>> MinMaxGradeByLecture(){
 		return managerChartRestService.selectMinMaxGradeByLecture();
 	}
 	// 과목별 학생 성적
-	@GetMapping("auth/manger/chart/GradeBySubject/{lectureNo}")
+	@GetMapping("auth/manger/chart/gradeBySubject/{lectureNo}")
 	public List<Map<String, Object>> GradeBySubject(
 			@PathVariable(name="lectureNo")int lectureNo){
 		return managerChartRestService.selectGradeBySubject(lectureNo);
 	}
 	// 학생별 출석률
-	@GetMapping("auth/manger/chart/AttendanceRateByStudent/{lectureNo}")
+	@GetMapping("auth/manger/chart/attendanceRateByStudent/{lectureNo}")
 	public List<Map<String, Object>> AttendanceRateByStudent(
 			@PathVariable(name="lectureNo")int lectureNo){
 		return managerChartRestService.selectAttendanceRateByStudent(lectureNo);
 	}
 	// 강좌별 별점 점수
-	@GetMapping("auth/manger/chart/StarRatingByLecture")
+	@GetMapping("auth/manger/chart/starRatingByLecture")
 	public List<Map<String, Object>> StarRatingByLecture(){
 		return managerChartRestService.selectStarRatingByLecture();
 	}
 	// 강좌별 출석 시도와 출석 횟수
-	@GetMapping("auth/manger/chart/AttendanceCountByLecture/{lectureNo}")
+	@GetMapping("auth/manger/chart/attendanceCountByLecture/{lectureNo}")
 	public List<Map<String, Object>> AttendanceCountByLecture(
 			@PathVariable(name="lectureNo")int lectureNo){
 		return managerChartRestService.selectAttendanceRateByStudent(lectureNo);
 	}
 	// 학생별 과제 점수
-	@GetMapping("auth/manger/chart/AttendanceCountByLecture/{reporNo}/{lectureNo}")
+	@GetMapping("auth/manger/chart/attendanceCountByLecture/{reporNo}/{lectureNo}")
 	public List<Map<String, Object>> AttendanceCountByLecture(
 			@PathVariable(name="reprotNo")int reprotNo,
 			@PathVariable(name="lectureNo")int lectureNo){
 		return managerChartRestService.selectReportCountByStudent(reprotNo, lectureNo);
 	}
 	// 강좌별 과제 제출률
-	@GetMapping("auth/manger/chart/ReportSubmissionRateByLecture/{lectureNo}")
+	@GetMapping("auth/manger/chart/reportSubmissionRateByLecture/{lectureNo}")
 	public List<Map<String, Object>> ReportSubmissionRateByLecture(
 			@PathVariable(name="lectureNo")int lectureNo){
 		return managerChartRestService.selectReportSubmissionRateByLecture(lectureNo);
 	}
 	// 월별 로그인 횟수
-	@GetMapping("auth/manger/chart/LoginCountByMonth/{year}/{month}")
+	@GetMapping("auth/manger/chart/loginCountByMonth/{year}/{month}")
 	public List<Map<String, Object>> LoginCountByMonth(
 			@PathVariable(name="year")int year,
 			@PathVariable(name="month")int month){
 		return managerChartRestService.selectLoginCountByMonth(year, month);
 	}
 	// 강의별 과제 점수 평균
-	@GetMapping("auth/manger/chart/AvgReportByLecture/{lectureNo}")
+	@GetMapping("auth/manger/chart/avgReportByLecture/{lectureNo}")
 	public List<Map<String, Object>> AvgReportByLecture(
 			@PathVariable(name="lectureNo")int lectureNo){
 		return managerChartRestService.selectAvgReportByLecture(lectureNo);
 	}
 	// 학생별 과제 점수 평균
-	@GetMapping("auth/manger/chart/AvgReprotByStudent/{lectureNo}")
+	@GetMapping("auth/manger/chart/avgReprotByStudent/{lectureNo}")
 	public List<Map<String, Object>> AvgReprotByStudent(
 			@PathVariable(name="lectureNo")int lectureNo){
 		return managerChartRestService.selectAvgReprotByStudent(lectureNo);
