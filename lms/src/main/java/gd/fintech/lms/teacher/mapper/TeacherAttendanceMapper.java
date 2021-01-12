0055a77561e50d59e9482b1601387d413fa7abad
rigizer <rigizer@gmail.com>
2021-01-12 11:27:52 +0900

    [한재용] 강사 출석 오류 수정
    : 1번 강좌를 제외한 나머지 강좌에서 1번 강좌의 출석 내용이 나오는 문제 수정
    - TeacherAttendanceMapper.java : 사용되지 않는 Mapper 삭제 (동기 출석 수정)
    - TeacherAttendanceMapper.xml : 출석 목록을 출력하는 쿼리 수정

--- lms/src/main/java/gd/fintech/lms/teacher/mapper/TeacherAttendanceMapper.java
+++ lms/src/main/java/gd/fintech/lms/teacher/mapper/TeacherAttendanceMapper.java
@@ -12,8 +12,4 @@ public interface TeacherAttendanceMapper {
 	// 강사 출석 조회
 	// 강좌 고유번호(lectureNo), 날짜(currentYear, currentMonth, currentDay)
 	List<Attendance> selectTeacherAttendanceListByDay(Map<String, Object> map);
-	
-	// 강사 출석 수정
-	// 출석 객체(Attendance)
-	int updateTeacherAttendance(Attendance attendance);
 }
